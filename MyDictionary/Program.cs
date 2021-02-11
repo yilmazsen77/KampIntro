3672af8e4ab70487fb861baab486951c3a52cf7a
Yılmaz Şen <yilmazsen77@outlook.com>
2021-02-11 21:05:25 +0300

    02112021

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1,"adfee");
+            myDictionary.Add(4,"adfd");
+            myDictionary.Add(77,"abv");
+            myDictionary.Add(1234,"ab");
+
+            myDictionary.ToList();
+        }
+    }
+}

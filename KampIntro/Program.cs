687b071eb3528a40b9c2072303753a0952e6ed06
Yılmaz Şen <yilmazsen77@outlook.com>
2021-02-11 20:56:09 +0300

    MyDictionary

---
+++ KampIntro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety: tip güvenliği
+            string kategoriEtiketi = "Kategori";
+
+
+
+            
+        }
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "a";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "b";
+
+            SepetManager sepet = new SepetManager();
+            sepet.Ekle(urun1);
+            sepet.Ekle(urun2);
+
+            sepet.Ekle2("aa","a",13);
+            
+        }
+    }
+}

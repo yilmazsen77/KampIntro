ad7135d036bdd5fe8d45a6d676a5a4060755769c
Yılmaz Şen <yilmazsen77@outlook.com>
2021-03-09 01:18:20 +0300

    ReCap

---
+++ ReCapProject/Business/Concrete/CarManager.cs
@@ -0,0 +1,50 @@
+﻿using ReCapProject.Business.Abstract;
+using ReCapProject.DataAccess.Abstract;
+using ReCapProject.DataAccess.InMemory;
+using ReCapProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReCapProject.Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        
+
+        public void Add(Car car)
+        {
+            _carDal.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+        }
+
+        public List<Car> GetAll()
+       
+        {
+            return _carDal.GetAll();
+        }
+
+                
+        public List<Car> GetById(Car car)
+        {
+            return _carDal.GetById(car);
+        }
+
+        public void Update(Car car)
+        {
+             _carDal.Update(car);
+
+        }
+    }
+}
---
+++ ReCapProject/Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using ReCapProject.Business.Concrete;
+using ReCapProject.DataAccess.InMemory;
+using ReCapProject.Entities;
+using System;
+
+namespace ReCapProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+
+        }
+    }
+}
